b068e2390344f9f8991da0594aae7ee27470183c
Dima Bolonikov <bolonickov.dmitry@icloud.com>
2019-07-06 06:36:03 +0300

    initial commit

---
+++ Jog Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jog Tracker
+//
+//  Created by Дмитрий Болоников on 7/6/19.
+//  Copyright © 2019 Дмитрий Болоников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
